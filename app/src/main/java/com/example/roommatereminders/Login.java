fe2d3776d1f52ca6a3ce43580f71e4dcdef28893
Justin5197 <justinlittle1234@gmail.com>
2019-04-03 19:10:07 -0400

    first commit

--- app/src/main/java/com/example/roommatereminders/Login.java
+++ app/src/main/java/com/example/roommatereminders/Login.java
@@ -9,6 +9,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //change made by justin
+        //change made by justin little
     }
 }
